ad3e63e2593993bd84a8e8159b860f25b219907b
wnkdsmm <lwjfkg@mail.ru>
2026-04-20 09:25:09 +0700

    add type annotations to column definitions constants

--- core/processing/steps/column_definitions.py
+++ core/processing/steps/column_definitions.py
@@ -1,4 +1,7 @@
-PROTECTION_REPORT_DEFAULTS = {
+from __future__ import annotations
+
+
+PROTECTION_REPORT_DEFAULTS: dict[str, str | bool] = {
     "profiling_candidate_to_drop": False,
     "mandatory_feature_detected": False,
     "protected_feature_id": "",
@@ -10,7 +13,7 @@ PROTECTION_REPORT_DEFAULTS = {
     "protected_from_drop": False,
 }
 
-PROTECTION_TEXT_COLUMNS = [
+PROTECTION_TEXT_COLUMNS: list[str] = [
     "protected_feature_id",
     "protected_feature_label",
     "protection_scope",
@@ -19,7 +22,7 @@ PROTECTION_TEXT_COLUMNS = [
     "protection_reason",
 ]
 
-PROTECTED_REPORT_COLUMNS = [
+PROTECTED_REPORT_COLUMNS: list[str] = [
     "column",
     "dtype",
     "profiling_candidate_to_drop",
